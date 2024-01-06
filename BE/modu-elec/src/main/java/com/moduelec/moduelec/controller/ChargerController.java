b3ea6a0fde146fbc4b92ca2581858682b06121a6
johan1103 <johan1103@naver.com>
2024-01-07 02:04:47 +0900

    [BE] Feat : Charger list api용 Controller 메서드 작성

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/ChargerController.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/ChargerController.java
@@ -1,2 +1,19 @@
-package com.moduelec.moduelec.controller;public class ChargerController {
+package com.moduelec.moduelec.controller;
+
+import com.moduelec.moduelec.dto.ChargerList;
+import com.moduelec.moduelec.service.ChargerListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChargerController {
+  private final ChargerListService chargerListService;
+  @GetMapping("/charger/list")
+  public ChargerList.ChargerListResponse searchChargerList(@RequestParam(value = "latitude")Double latitude,
+                                                        @RequestParam(value = "longitude")Double longitude){
+    return chargerListService.searchByDistance(longitude,latitude);
+  }
 }
