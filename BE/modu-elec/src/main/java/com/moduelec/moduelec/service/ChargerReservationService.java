cd973d0e32d4fefe8bfa3872c2e9035181b3a54a
johan1103 <johan1103@naver.com>
2024-01-07 04:29:26 +0900

    [BE] Feat : 예약 서비스 로직 작성

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerReservationService.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerReservationService.java
@@ -1,2 +1,41 @@
-package com.moduelec.moduelec.service;public class ChargerReservationService {
+package com.moduelec.moduelec.service;
+
+import com.moduelec.moduelec.entity.ChargerInfo;
+import com.moduelec.moduelec.entity.Event;
+import com.moduelec.moduelec.entity.EventHour;
+import com.moduelec.moduelec.entity.User;
+import com.moduelec.moduelec.repository.ChargerInfoRepository;
+import com.moduelec.moduelec.repository.EventHourRepository;
+import com.moduelec.moduelec.repository.EventRepository;
+import com.moduelec.moduelec.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChargerReservationService {
+  private final EventRepository eventRepository;
+  private final EventHourRepository eventHourRepository;
+  private final UserRepository userRepository;
+  private final ChargerInfoRepository chargerInfoRepository;
+
+  @Transactional
+  public void createEvent(int startHour, int duration, Long userId, Long chargerInfoId){
+    User user = userRepository.findById(userId).orElseThrow(()->new RuntimeException("invalid user id ["+userId+"]"));
+    ChargerInfo chargerInfo = chargerInfoRepository
+            .findById(chargerInfoId).orElseThrow(()->new RuntimeException("invalid charger info id ["+chargerInfoId+"]"));
+    Event event = new Event(null,user, LocalDateTime.now());
+    eventRepository.save(event);
+
+    List<EventHour> eventHours = new ArrayList<>();
+    for(int i=0;i<duration;i++){
+      eventHours.add(new EventHour(null,chargerInfo,startHour+i,event));
+    }
+    eventHourRepository.saveAll(eventHours);
+  }
 }
