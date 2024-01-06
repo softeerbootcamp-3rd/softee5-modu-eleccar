137c570d8bb1dc53fc0e8191ab55690e283b2c2a
johan1103 <johan1103@naver.com>
2024-01-07 02:03:05 +0900

    [BE] Feat : dto 반환용 인터페이스(aop) 작성

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/dto/ChargerResponseInterface.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/dto/ChargerResponseInterface.java
@@ -1,2 +1,10 @@
-package com.moduelec.moduelec.dto;public interface ChargerResponseInterface {
+package com.moduelec.moduelec.dto;
+
+public interface ChargerResponseInterface {
+  Long getChargerInfoId();
+  String getType();
+  Double getDistance();
+  Integer getPricePerHour();
+  Integer getStartHour();
+  Integer getEndHour();
 }
