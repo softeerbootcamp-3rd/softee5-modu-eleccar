2dc9cb313e7be96191d6ef4557566867f4ee278d
johan1103 <johan1103@naver.com>
2024-01-07 04:29:07 +0900

    [BE] Feat : User,Event,EventHour용 repository 생성

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/repository/EventRepository.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/repository/EventRepository.java
@@ -1,2 +1,7 @@
-package com.moduelec.moduelec.repository;public interface EventRepository {
+package com.moduelec.moduelec.repository;
+
+import com.moduelec.moduelec.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event,Long> {
 }
--- BE/modu-elec/src/main/java/com/moduelec/moduelec/repository/UserRepository.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.moduelec.moduelec.repository;public interface UserRepository {
+package com.moduelec.moduelec.repository;
+
+import com.moduelec.moduelec.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
