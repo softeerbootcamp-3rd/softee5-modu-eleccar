e12063aaeb9275f3d3bca011a276249d1f5a4655
admin <lejs1008@naver.com>
2024-01-06 19:45:15 +0900

    [BE] Feat : ChargerInfo Repository 생성

---
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/repository/ChargerInfoRepository.java
@@ -0,0 +1,7 @@
+package com.moduelec.moduelec.repository;
+
+import com.moduelec.moduelec.entity.ChargerInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChargerInfoRepository extends JpaRepository<ChargerInfo, Long> {
+}
