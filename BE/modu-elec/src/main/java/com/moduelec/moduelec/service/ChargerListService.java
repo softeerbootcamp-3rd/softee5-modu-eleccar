f40138a4aa292550d4f0ef89aaea367362f18905
johan1103 <johan1103@naver.com>
2024-01-07 02:06:16 +0900

    [BE] Feat : Service Layer 작성, stream으로 database에게 받은 dto를 Controller response용 dto로 전환

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerListService.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerListService.java
@@ -1,2 +1,29 @@
-package com.moduelec.moduelec.service;public class ChargerListService {
+package com.moduelec.moduelec.service;
+
+import com.moduelec.moduelec.dto.ChargerList;
+import com.moduelec.moduelec.dto.ChargerResponseInterface;
+import com.moduelec.moduelec.repository.ChargerInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.moduelec.moduelec.dto.ChargerList.*;
+
+@Service
+@RequiredArgsConstructor
+public class ChargerListService {
+  private final ChargerInfoRepository chargerInfoRepository;
+
+  public ChargerListResponse searchByDistance(Double longitude, Double latitude){
+    List<ChargerResponseInterface> chargerInterfaces = chargerInfoRepository.
+            findAllByDistance(latitude,longitude);
+    List<ChargerResponse> chargerResponses = chargerInterfaces.stream().map(ChargerResponse::of).toList();
+
+    return ChargerListResponse.of(chargerResponses);
+  }
 }
