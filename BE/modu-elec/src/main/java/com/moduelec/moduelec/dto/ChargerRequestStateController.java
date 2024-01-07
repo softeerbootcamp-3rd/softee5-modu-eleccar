419a64d9f28723d5ddf0d1188426a6e7e89711c0
admin <lejs1008@naver.com>
2024-01-07 21:11:50 +0900

    ChargerRequestStateController 구현

---
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/dto/ChargerRequestStateController.java
@@ -0,0 +1,23 @@
+package com.moduelec.moduelec.dto;
+
+import com.moduelec.moduelec.service.ChargerRequestStateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChargerRequestStateController {
+    private final ChargerRequestStateService chargerRequestStateService;
+
+    @GetMapping("/charger/request/state")
+    public ResponseEntity<ChargerRequestState> getChargerRequestState(
+            @RequestParam("eventId") Long id
+    ) {
+        ChargerRequestState chargerRequestState = chargerRequestStateService.getChargerRequestState(id);
+        return new ResponseEntity<>(chargerRequestState, HttpStatus.OK);
+    }
+}
