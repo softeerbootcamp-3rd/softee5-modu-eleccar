3fa1475239a7cef8856285f7ee7158045057539a
admin <lejs1008@naver.com>
2024-01-07 17:54:45 +0900

    [BE] Feat : ChargerState Service계층 도입

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/ChargerStateController.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/ChargerStateController.java
@@ -4,6 +4,7 @@ import com.moduelec.moduelec.dto.ChargerState;
 import com.moduelec.moduelec.entity.ChargerInfo;
 import com.moduelec.moduelec.repository.ChargerInfoRepository;
 import com.moduelec.moduelec.repository.EventHourRepository;
+import com.moduelec.moduelec.service.ChargerStateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,26 +18,9 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ChargerStateController {
 
-    private final ChargerInfoRepository chargerInfoRepository;
-    private final EventHourRepository eventHourRepository;
+    private final ChargerStateService chargerStateService;
     @GetMapping("/charger/state")
-    public List<ChargerState> getChargerState(
-            @RequestParam("chargerInfoId") Long id
-    ) {
-        ChargerInfo chargerInfo = chargerInfoRepository.findById(id).get();
-        Integer startHour = chargerInfo.getStartHour();
-        Integer endHour = chargerInfo.getEndHour();
-        List<Integer> reservedStartHours = eventHourRepository.findReservedStartHours(id);
-
-        List<ChargerState> chargerStates = new ArrayList<>();
-        for(int i=0; i<24; i++) {
-            ChargerState chargerState = new ChargerState(i, false);
-            if(i>=startHour && i<endHour && !reservedStartHours.contains(i)) {
-                chargerState.setAvailable(true);
-            }
-            chargerStates.add(chargerState);
-        }
-
-        return chargerStates;
+    public List<ChargerState> getChargerState(@RequestParam("chargerInfoId") Long id) {
+        return chargerStateService.getChargerState(id);
     }
 }
---
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerStateService.java
@@ -0,0 +1,38 @@
+package com.moduelec.moduelec.service;
+
+import com.moduelec.moduelec.dto.ChargerState;
+import com.moduelec.moduelec.entity.ChargerInfo;
+import com.moduelec.moduelec.repository.ChargerInfoRepository;
+import com.moduelec.moduelec.repository.EventHourRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChargerStateService {
+
+    private final ChargerInfoRepository chargerInfoRepository;
+    private final EventHourRepository eventHourRepository;
+
+    public List<ChargerState> getChargerState(Long id) {
+        ChargerInfo chargerInfo = chargerInfoRepository.findById(id).get();
+
+        Integer startHour = chargerInfo.getStartHour();
+        Integer endHour = chargerInfo.getEndHour();
+        List<Integer> reservedStartHours = eventHourRepository.findReservedStartHours(id);
+
+        List<ChargerState> chargerStates = new ArrayList<>();
+        for (int i = 0; i < 24; i++) {
+            ChargerState chargerState = new ChargerState(i, false);
+            if (i >= startHour && i < endHour && !reservedStartHours.contains(i)) {
+                chargerState.setAvailable(true);
+            }
+            chargerStates.add(chargerState);
+        }
+
+        return chargerStates;
+    }
+}
