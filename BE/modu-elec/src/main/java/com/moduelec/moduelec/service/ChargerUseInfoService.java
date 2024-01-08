8e78d8384964b7d8fc95442b20d807e7d05bdf5f
admin <lejs1008@naver.com>
2024-01-08 15:39:48 +0900

    [BE] Feat : 예상 충전량, 대여 금액 계산 로직 추가

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerUseInfoService.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/service/ChargerUseInfoService.java
@@ -24,9 +24,15 @@ public class ChargerUseInfoService {
         if(event.getReservation().getUser().getId().equals(userId)) {
             ChargerInfo chargerInfo = eventHours.get(0).getChargerInfo();
 
-            dto.setReservedStartHour(eventHours.get(0).getStartHour());
-            dto.setReservedEndHour(eventHours.get(eventHours.size()-1).getStartHour() + 1);
+            Integer reservedStartHour = eventHours.get(0).getStartHour();
+            Integer reservedEndHour = eventHours.get(eventHours.size()-1).getStartHour() + 1;
+
+
+            dto.setReservedStartHour(reservedStartHour);
+            dto.setReservedEndHour(reservedEndHour);
             dto.setAddress(chargerInfo.getAddress());
+            dto.setExpectedChargeAmount(chargerInfo.getSpeed() * (reservedEndHour - reservedStartHour));
+            dto.setTotalPrice(chargerInfo.getPricePerHour() * (reservedEndHour - reservedStartHour));
             dto.setMessage(chargerInfo.getMessage());
         }
 
