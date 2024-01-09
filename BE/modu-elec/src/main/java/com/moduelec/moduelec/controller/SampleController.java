b0e8ef10fca3db45443815adc4141c6703243479
johan1103 <johan1103@naver.com>
2024-01-09 19:18:46 +0900

    [BE] Feat : sample message push API 생성

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/SampleController.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/controller/SampleController.java
@@ -1,7 +1,12 @@
 package com.moduelec.moduelec.controller;
 
+import com.moduelec.moduelec.dto.FcmServiceDto;
+import com.moduelec.moduelec.entity.Event;
 import com.moduelec.moduelec.entity.Sample;
+import com.moduelec.moduelec.entity.User;
 import com.moduelec.moduelec.repository.SampleRepository;
+import com.moduelec.moduelec.repository.UserRepository;
+import com.moduelec.moduelec.service.FcmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +19,8 @@ import java.util.List;
 public class SampleController {
 
   private final SampleRepository sampleRepository;
+  private final FcmService fcmService;
+  private final UserRepository userRepository;
 
   @GetMapping("/hello-world")
   public List<Sample> getAllSample(){
@@ -22,5 +29,10 @@ public class SampleController {
   @PostMapping("/hello-world")
   public void insert(){
     sampleRepository.save(new Sample(null,"abc"));
+  }
+  @PostMapping("/sample/message")
+  public void sendPushMessage(){
+    Event event = Event.create(userRepository.findById(1L).orElseThrow(()->new RuntimeException("invalid user id")));
+    fcmService.sendByToken(FcmServiceDto.create(event.getId()),1L);
   }
 }
