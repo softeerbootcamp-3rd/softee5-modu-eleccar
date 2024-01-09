6e0e6b3acdd822dc31ef4e1ab3bdcc5d51a40108
johan1103 <johan1103@naver.com>
2024-01-09 15:44:15 +0900

    [BE] Fix : fcm 초기화 설정

--- BE/modu-elec/src/main/java/com/moduelec/moduelec/configuration/FCMConfig.java
+++ BE/modu-elec/src/main/java/com/moduelec/moduelec/configuration/FCMConfig.java
@@ -3,29 +3,33 @@ package com.moduelec.moduelec.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.gson.JsonArray;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 @Component
 @Slf4j
 public class FCMConfig {
+  @Value("${fcm.path}")
+  private String fcmPath;
   @PostConstruct
   public void getFcmCredential(){
     try {
-      ClassPathResource resource = new ClassPathResource("/firebase-adminsdk.json");
-      InputStream refreshToken = resource.getInputStream();
+      // JSON 파일 읽기
+      InputStream refreshToken = new FileInputStream(fcmPath);
 
       FirebaseOptions options = FirebaseOptions.builder()
               .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
 
       FirebaseApp.initializeApp(options);
       log.info("Fcm Setting Completed");
-    } catch (IOException e) {
+    } catch (Exception e) {
       throw new RuntimeException(e.getMessage());
     }
   }
